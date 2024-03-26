a5a13b4f485a91f3b55e9ed49f0f1045b6d2d980
MikeMaesPXL <11901518@student.pxl.be>
2024-03-26 15:47:39 +0100

    Removed comments, fixed liquid fill animation and added margins to certains text/buttons.

--- gsap.js
+++ gsap.js
@@ -1,28 +1,8 @@
-// var allElements = document.querySelectorAll("body *");
-
 document.addEventListener("DOMContentLoaded", function () {
-    // Code hier zal worden uitgevoerd wanneer de DOM geladen is
     gsap.from(".fade-up", {
         duration: 1,
         opacity: 0,
         y: 50,
-        stagger: 0.1 // Vertraging tussen animaties van opeenvolgende elementen (0.1 seconden)
+        stagger: 0.1
     });
-        // gsap.from(".fade-up", {
-        //     duration: .6,
-        //     fade-up: .9,
-        //     animation: 0,
-        //     stagger: 0.1 // Vertraging tussen animaties van opeenvolgende elementen (0.1 seconden)
-        // });
 });
-
-// Selecteer alle elementen binnen de body van het document
-// var fade-up = document.getElementsByClassName(".fade-up");
-
-// Pas een animatie toe op elk individueel element met een vertraging
-// gsap.from(".fade-up", {
-//     duration: 1,
-//     animation: 0,
-//     y: 50,
-//     stagger: 0.1 // Vertraging tussen animaties van opeenvolgende elementen (0.1 seconden)
-// });
